686401536beca7fe957e5e352248e22e76cac416
zerrinayz <zerrinayaz76@gmail.com>
2025-03-08 15:57:27 +0300

    started cpp01 ex03

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -13,5 +13,6 @@ int main()
     std::cout << "The value of the string variable: " << str << std::endl;
     std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
     std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+
     return 0;
 }
