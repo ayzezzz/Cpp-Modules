276213fcf64c9ce27b18dcfc983488164e2f49d4
zerrinayz <zerrinayaz76@gmail.com>
2025-03-22 18:47:51 +0300

    code edited cpp04

--- cpp04/ex00/Animal.cpp
+++ cpp04/ex00/Animal.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zayaz <zayaz@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/22 15:59:59 by zayaz             #+#    #+#             */
+/*   Updated: 2025/03/22 17:40:30 by zayaz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Animal.hpp"
 
 Animal::Animal() : type("Animal") {
@@ -8,6 +20,7 @@ Animal::Animal(const Animal& other) {
     *this = other;
     std::cout << "Animal copied" << std::endl;
 }
+
 
 Animal& Animal::operator=(const Animal& other) {
     if (this != &other)
--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zayaz <zayaz@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/22 16:45:14 by zayaz             #+#    #+#             */
+/*   Updated: 2025/03/22 17:51:28 by zayaz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Dog.hpp"
 #include "Cat.hpp"
 #include "Brain.hpp"
@@ -6,7 +18,7 @@ int main()
 {
     std::cout << "=== Abstract Class Test ===" << std::endl;
 
-    //AAnimal *meta = new AAnimal();
+   // AAnimal *meta = new AAnimal();
 
     const AAnimal *j = new Dog();
     const AAnimal *i = new Cat();
